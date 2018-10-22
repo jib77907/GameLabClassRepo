52ad8b5985c7fac192345378c334fdad64bf04b6
jib77907 <leekp733@newschool.edu>
2018-10-22 00:13:01 -0400

    midterm update

---
+++ Midterm-ChickenOrEgg/Assets/chickenController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chickenController : MonoBehaviour {
+
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+        moveSpeed = 0.05f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * moveSpeed);
+        }
+    }
+}
